264903c94fe322a772d5851f2c4ee632dfbac3b4
Prashanth <prashanth.royal2046@gmail.com>
2021-05-09 11:06:42 +0530

    Added: Length of Line

--- LineComparision.java
+++ LineComparision.java
@@ -1,1 +1,12 @@
-#Welcome to Line Comparision problem solution
+
+	public class LineComparision {
+
+		public static void main(String[] args) {
+		//variables
+		int x1=1, y1=2, x2=4, y2=5;
+		//Computation
+		double length = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		System.out.println(length);
+	}
+}
+
